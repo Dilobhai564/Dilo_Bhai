4c6eca1789f1efdc04e3f91035d3a57d76b00451
Dilobhai564 <127302754+Dilobhai564@users.noreply.github.com>
2023-03-08 17:09:21 +0500

    Create Dilo_Bhai.py
    pkg update
     pkg upgrade
     pkg install python
     pkg install git
     pip install requests
     pip install bs4
     pip install futures
     pip install mechanize
     cd $HOME 
     rm -rf DBD
     git clone https://github.com/Dilo_Bhai/DBD
    cd DBD
     python DBD.py

---
+++ Dilo_Bhai.py
@@ -0,0 +1,64 @@
+
+____Welcome____
+
+
+<a href="https://github.com/Dilo_Bhai/followers">
+<img title="Followers" src="https://img.shields.io/github/followers/Dilo_Bhai?label=Followers&color=green&style=flat-square"></a>
+
+<br>
+  <a href="https://github.com/Dilo_Bhai/termux-style/stargazers/">
+  <a href="https://github.com/Dilo_Bhai/DBD">
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/Dilo_Bhai/DBD.svg"/>
+  </a>
+<br>
+  <a href="https://github.com/Dilo_Bhai/DBD">
+    <img alt="Language" src="https://img.shields.io/github/languages/count/Dilo_Bhai/DBD.svg"/>
+  </a>
+  <a href="https://github.com/Dilo_Bhai/DBD">
+    <img alt="Starts" src="https://img.shields.io/github/stars/Dilo_Bhai/DBD.svg"/>
+  </a>
+<br>
+<a href="https://github.com/Dilo_Bhai/DBD">
+    <img alt="Repo Size" src="https://img.shields.io/github/repo-size/Dilo_Bhai/DBD.svg"/>
+  </a>
+<br>
+<a href="https://github.com/Dilo_Bhai/DBD">
+    <img alt="Top Language" src="https://img.shields.io/github/languages/top/Dilo_Bhai/DBD.svg"/> <a                                                                                                        href="https://github.com/Azim-vau/fcpromax">
+    <img alt="Forks" src="https://img.shields.io/github/forks/Dilo_Bhai/DBD.svg"/>
+  </a>
+</div>
+
+</br>
+<p align="center">
+      FACEBOOK ACCOUNT CLONER
+</p>
+  
+#### INSTALL TOOL ON TERMUX
+```python
+ pkg update
+ pkg upgrade
+ pkg install python
+ pkg install git
+ pip install requests
+ pip install bs4
+ pip install futures
+ pip install mechanize
+ cd $HOME 
+ rm -rf DBD
+ git clone https://github.com/Dilo_Bhai/DBD
+```
+#### RUN SCRIPT
+```python
+ cd DBD
+ python DBD.py
+```
+
+
+#### MY SOCIAL MEDIA
+
+[![](https://img.shields.io/badge/Github-black?logo=Github&logoColor=red&labelColor=black)](https://github.com/Dilobhai564-DBD) <br>
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=red&labelColor=blue)](https://www.facebook.com/profile.php?id=100089109290489&mibextid=ZbWKwL) <br>
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=yellow&labelColor=red)](https://www.facebook.com/profile.php?id=100089109290489&mibextid=ZbWKwL) <br>
+
+<h2> THANKS FOR VISIT <h2\>
+
